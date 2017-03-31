da725aec7fdb01f370d0a0a9571da293e0966029
Ryan Teves <rteves@ucsc.edu>
2017-03-30 23:42:35 -0700

    more progress, setting up execution of commands

--- asgn1/sbi.scm
+++ asgn1/sbi.scm
@@ -40,6 +40,8 @@
                   (close-input-port inputfile)
                          program))))
 (define *symbol-table* (make-hash))
+(define *function-table* (make-hash))
+(define *label-table* (make-hash)) ; label hash table
 (define (symbol-put! key value)
 	(hash-set! *symbol-table* key value))
 ; print sbir file and commands
@@ -51,9 +53,53 @@
     (map (lambda (line) (printf "~s~n" line)) program)
     (printf ")~n"))
 
-(define l-hash (make-hash)) ;command hash table
+(define (h_eval expr)
+	(printf "~s~n" expr))
 
-(define (hash-labels program)
+(define (sb_print expr)
+	(map (lambda (x) (display (h_eval x))) expr)
+	(newline))
+
+(define (exe-line instr program line-num)
+	(when (not (hash-has-key? *function-hash* (car instr)))
+		(die "~s is not a valid instruction." (car instr)))
+	(cond
+		((eq? (car instr) 'goto)
+		(read-cmd program (hash-ref *label-hash* (cadr instr))))
+		((eq? (car instr) 'if)
+		(if (h_eval (car (cdr instr)))
+			(read-cmd program (hash-ref *label-table* (cadr (cdr instr))))
+			(read-cmd program (+ line-num 1))))
+		((eq? (car instr) 'print)
+		(if (null? (cdr instr))
+			(newline)
+			(sb_print (cdr instr))) ; Bad ident
+			(read-cmd program (+ line-num 1)))
+		(else
+			((hash-ref *function-table* (car instr)) (cdr instr))
+			(read-cmd program (+ line-num 1)))))
+
+(define (read-cmd program line-num) ; line number
+	(when (> (length program) line-num)
+	(printf "DEBUG: Executing line ~a of ~a.~n"
+		line-num (length program))
+	(printf "	~s~n" (list-ref program line-num))
+	(let((line (list-ref program line-num)))
+	(cond
+		((= (length line) 3)
+		(set! line (cddr line))
+		;(printf "test 1~n")
+		(exe-line (car line) program line-num))
+		((and (= (length line) 2) (list? (cadr line)))
+		(set! line (cdr line))
+		;(printf "test 2~n")
+		(exe-line (car line) program line-num))
+		(else
+			(read-cmd program (+ line-num 1)))
+		))))
+
+
+(define (label-hash program)
 	(printf "Hashing labels:~n")
  	(printf "==================================================~n")
 	(map (lambda (line)
@@ -63,11 +109,11 @@
 					(not (list? (cadr line)))))
 			(printf "~a: ~s~n" (- (car line) 1) (cadr line))
 			(printf "    ~s~n" (list-ref program (- (car line) 1)))
-			(hash-set! l-hash (cadr line) (- (car line) 1 ))
+			(hash-set! *label-table* (cadr line) (- (car line) 1 ))
 			))) program)
 	(printf "==================================================~n")
  	(printf "Dumping label table...~n")
-	(map (lambda (el) (printf "~s~n" el))(hash->list l-hash))
+	(map (lambda (el) (printf "~s~n" el))(hash->list *label-table*))
 )
 
 ; start of program
@@ -82,9 +128,10 @@
                (program (readlist-from-inputfile sbprogfile)))
 		; print filename and commands in sbir file
               (write-program-by-line sbprogfile program)
-		; get program commands
-		(hash-labels program)
-
+		; get labels of program 
+		(label-hash program)
+		; read commands
+		(read-cmd program 0)
 )))
 
 (main (vector->list (current-command-line-arguments)))
