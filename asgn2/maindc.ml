f08e0f9ea09accab2dcc950512bea023f2dac377
Ky Nguyen <kymnguye@ucsc.edu>
2017-05-03 16:49:50 -0700

    fixed l, s functions

--- asgn2/maindc.ml
+++ asgn2/maindc.ml
@@ -10,7 +10,7 @@ open Scanner
 type stack_t = Bigint.bigint Stack.t
 let push = Stack.push
 let pop = Stack.pop
-let regs = Hashtbl.create 10
+let registers = Hashtbl.create 10
 
 let ord thechar = int_of_char thechar
 type binop_t = bigint -> bigint -> bigint
@@ -32,12 +32,11 @@ let print_stackempty () = printf "stack empty\n%!"
 let executereg (thestack: stack_t) (oper: char) (reg: int) =
   try match oper with
     | 'l' -> 
-      if (Hashtbl.mem regs reg)
-      then push (Hashtbl.find regs reg) thestack
+      if (Hashtbl.mem registers reg)
+        then push (Hashtbl.find registers reg) thestack
       else printf "ocamldc: register '%c' (0%o) is empty\n%!"
         (char_of_int reg) reg
-    | 's' ->
-      Hashtbl.replace regs reg (pop thestack)
+    | 's' -> Hashtbl.replace registers reg (pop thestack)
     | _   -> printf "0%o 0%o is unimplemented\n%!" (ord oper) reg
       with Stack.Empty -> print_stackempty()
 
